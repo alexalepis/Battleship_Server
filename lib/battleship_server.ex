efdd721e226cf0a6a8fe22b4abea91a7b11363ef
alexalepis <aalepis@NT-21.tcb.local>
2018-03-06 17:50:05 +0200

    build(server) added deps

--- lib/battleship_server.ex
+++ lib/battleship_server.ex
@@ -1,18 +1,4 @@
 defmodule BattleshipServer do
-  @moduledoc """
-  Documentation for BattleshipServer.
-  """
 
-  @doc """
-  Hello world.
 
-  ## Examples
-
-      iex> BattleshipServer.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
